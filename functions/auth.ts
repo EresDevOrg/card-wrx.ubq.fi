00407bb29875519cbabc411886ef8af560ba9b14
EresDev <eresdev@hotmail.com>
2025-03-18 12:24:44 +0500

    feat: get wirex auth token

--- functions/auth.ts
+++ functions/auth.ts
@@ -1,3 +1,12 @@
-export async function onRequest(): Promise<Response> {
-  return Response.json({ message: "test" }, { status: 200 });
+import { getAccessToken } from "./shared";
+
+export async function onRequest(ctx): Promise<Response> {
+  try {
+    const accessToken = await getAccessToken(ctx.env);
+
+    return Response.json(accessToken, { status: 200 });
+  } catch (e) {
+    console.log("Getting access token failed. Error:", e);
+    return Response.json({ message: "Getting access token failed" }, { status: 500 });
+  }
 }
---
+++ functions/shared.ts
@@ -0,0 +1,44 @@
+import { WirexAuthResponse } from "./types";
+
+export const WIREX_AUTH_URL_SANDBOX = "https://wirex-pay-dev.eu.auth0.com/oauth/token";
+export const WIREX_AUTH_URL_PRODUCTION = "https://wirex-pay-dev.eu.auth0.com/oauth/token";
+export const WIREX_API_URL_SANDBOX = "https://api-business.wirexpaychain.tech";
+export const WIREX_API_URL_PRODUCTION = "https://api-business.wirexpaychain.tech";
+
+export interface Env {
+  USE_WIREX_SANDBOX: string;
+  WIREX_CLIENT_ID: string;
+  WIREX_CLIENT_SECRET: string;
+}
+
+export interface AccessToken {
+  token: string;
+  isSandbox: boolean;
+  expiresIn: number; // in seconds
+}
+
+export async function getAccessToken(env: Env): Promise<AccessToken> {
+  console.log("Using Wirex Sandbox:", env.USE_WIREX_SANDBOX !== "false");
+  const options = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      client_id: env.WIREX_CLIENT_ID,
+      client_secret: env.WIREX_CLIENT_SECRET,
+      grant_type: "client_credentials",
+      audience: env.USE_WIREX_SANDBOX === "false" ? WIREX_API_URL_PRODUCTION : WIREX_API_URL_SANDBOX,
+    }),
+  };
+
+  const authUrl = env.USE_WIREX_SANDBOX === "false" ? WIREX_AUTH_URL_PRODUCTION : WIREX_AUTH_URL_SANDBOX;
+  const res = await fetch(authUrl, options);
+  if (res.status == 200) {
+    const successResponse = (await res.json()) as WirexAuthResponse;
+    return {
+      token: successResponse.access_token,
+      isSandbox: env.USE_WIREX_SANDBOX !== "false",
+      expiresIn: successResponse.expires_in,
+    };
+  }
+  throw new Error(`Getting access token failed: ${JSON.stringify(await res.json())}`);
+}
