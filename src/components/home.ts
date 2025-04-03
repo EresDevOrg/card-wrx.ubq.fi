a799c724e8c472a2f50ec7b3e661073e8901f6cb
EresDev <eresdev@hotmail.com>
2025-04-03 20:23:28 +0500

    feat: add home page with link to register page

--- src/components/home.ts
+++ src/components/home.ts
@@ -1,4 +1,6 @@
+import { appState } from "../main";
 import { getCardImage } from "./card-svg";
+import { showToast } from "./toaster";
 
 export function createHomePage(): string {
   return `
@@ -6,7 +8,21 @@ export function createHomePage(): string {
       <div>
         ${getCardImage()}  
         <h4>Seamless. Secure. Ubiquitous.</h4>
+        <h2><a href="#/register" id="proceed-to-register">Register to get your UbiquiCard</a></h2>
       </div>
     </div>
   `;
+}
+
+export function addHomeEvents() {
+  const proceedToRegister = document.getElementById("proceed-to-register");
+
+  if (proceedToRegister) {
+    proceedToRegister.addEventListener("click", (event) => {
+      if (!appState.getIsConnectedState()) {
+        event.preventDefault();
+        showToast({ message: "Please connect your wallet first.", type: "error" });
+      }
+    });
+  }
 }
