3b96014d84f03bed6f6f1a55d7c9418522e95ff9
EresDev <eresdev@hotmail.com>
2025-04-17 16:59:16 +0500

    fix: prevent auth if already authenticated

--- src/shared/user-session.ts
+++ src/shared/user-session.ts
@@ -4,51 +4,54 @@ import { Session } from "./types";
 import { getWirexApiUrl, sign } from "./utils";
 
 export async function authenticate(wallet: string): Promise<void> {
-  let signature;
-  try {
-    signature = await sign(`Authentication request for ${wallet.toLowerCase()}`);
-  } catch (e) {
-    showToast({
-      message: "Signature is required to use the app.",
-      type: "error",
-    });
-    console.error("Error signing message: ", e);
-    return;
-  }
-
-  const responseAuth = await fetch(`${backendBaseUrl}/user-auth`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-    body: JSON.stringify({
-      wallet: wallet,
-      signature: signature,
-    }),
-  });
-
-  const session: Session = { wallet: wallet, ...(await responseAuth.json()) };
-
-  console.log("Session: ", session);
-
-  if (responseAuth.ok) {
-    const cardsUrl = `${getWirexApiUrl("/api/v1/cards?page_number=0&page_size=10", session.isSandbox)}`;
-    const cardsResponse = await fetch(cardsUrl, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${session.access_token}`,
-        Accept: "application/json",
-        "X-User-Wallet": session.wallet,
-      },
-    });
-
-    const cards = await cardsResponse.json();
-    console.log("cards", cards);
-    if (cards.data) {
-      session.cards = cards.data;
+  const session = getSession();
+  if (session) {
+    let signature;
+    try {
+      signature = await sign(`Authentication request for ${wallet.toLowerCase()}`);
+    } catch (e) {
+      showToast({
+        message: "Signature is required to use the app.",
+        type: "error",
+      });
+      console.error("Error signing message: ", e);
+      return;
     }
-    localStorage.setItem("session", JSON.stringify(session));
+
+    const responseAuth = await fetch(`${backendBaseUrl}/user-auth`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        wallet: wallet,
+        signature: signature,
+      }),
+    });
+
+    const session: Session = { wallet: wallet, ...(await responseAuth.json()) };
+
+    console.log("Session: ", session);
+
+    if (responseAuth.ok) {
+      const cardsUrl = `${getWirexApiUrl("/api/v1/cards?page_number=0&page_size=10", session.isSandbox)}`;
+      const cardsResponse = await fetch(cardsUrl, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${session.access_token}`,
+          Accept: "application/json",
+          "X-User-Wallet": session.wallet,
+        },
+      });
+
+      const cards = await cardsResponse.json();
+      console.log("cards", cards);
+      if (cards.data) {
+        session.cards = cards.data;
+      }
+      localStorage.setItem("session", JSON.stringify(session));
+    }
   }
 }
 
