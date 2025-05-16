e7142ac6d9bae1e4451ef61de251394d590dd0f8
EresDev <eresdev@hotmail.com>
2025-05-16 13:07:23 +0500

    docs: use correct & explicit types

--- src/shared/account-abstraction/utils.ts
+++ src/shared/account-abstraction/utils.ts
@@ -2,7 +2,7 @@ import { signerToEcdsaValidator } from "@zerodev/ecdsa-validator";
 import { createKernelAccount, createKernelAccountClient, createZeroDevPaymasterClient } from "@zerodev/sdk";
 import { getEntryPoint, KERNEL_V3_1 } from "@zerodev/sdk/constants";
 import { Signer } from "@zerodev/sdk/types";
-import { createPublicClient, http } from "viem";
+import { createPublicClient, EIP1193Provider, http } from "viem";
 import { baseSepolia } from "viem/chains";
 import { PUBLIC_RPC, ZERODEV_RPC } from "./constants";
 
@@ -55,9 +55,19 @@ export async function getKernelAccountClient(signer: Signer) {
   return client;
 }
 
+export function getEip1193Provider(): EIP1193Provider {
+  if (!window.ethereum) {
+    throw new Error("No Ethereum provider found");
+  }
+  // window.ethereum is EIP1193Provider, accepted as Signer by ZeroDev/sdk
+  // https://docs.zerodev.app/sdk/signers/eoa#eip-1193-provider-integration
+  return window.ethereum as EIP1193Provider;
+}
+
 export async function getSmartAccountAddress(): Promise<string | null> {
   try {
-    const client = await getKernelAccountClient(window.ethereum as Signer);
+    const eip1193Provider = getEip1193Provider();
+    const client = await getKernelAccountClient(eip1193Provider as Signer);
 
     const smartAccountAddress = client.account.address;
     console.log("Smart Account Address:", smartAccountAddress);
